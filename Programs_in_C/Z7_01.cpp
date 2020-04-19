175c6c8cd31eaa7b3e89d20741aeff65397bed95
DarkHunter <konrad.romanski4@gmail.com>
2020-04-20 01:50:12 +0200

    Z7_01 finished

--- Programs_in_C/Z7_01.cpp
+++ Programs_in_C/Z7_01.cpp
@@ -4,27 +4,77 @@
 struct Pracownik
 {
     char Nazwisko[32];
-    int Pensja;
+    double Pensja;
 };
 
 char vchar();
 
-void opcja_N(Pracownik* TabPrac, int pozycja)
+void opcja_R(Pracownik* TabPrac, int &pozycja)
+{
+    char nazwa_pliku[32] = "";
+    FILE* file_in = NULL;
+    Pracownik temp;
+
+
+    
+    while (file_in == NULL)
+    {
+        printf("Podaj nazwe pliku: ");
+        scanf_s("%s", &nazwa_pliku, 31);
+        fopen_s(&file_in, nazwa_pliku, "r");
+    }
+
+    while (!feof(file_in))
+    {
+        fscanf_s(file_in, "%s", &temp.Nazwisko, 31);
+        fscanf_s(file_in, "%lf", &temp.Pensja);
+
+        TabPrac[pozycja] = temp;
+        pozycja++;
+    }
+
+    fclose(file_in);
+}
+
+void opcja_N(Pracownik* TabPrac, int &pozycja)
 {
     Pracownik nowy = {"", 0};
     printf("Podaj Nazwisko i Pensje nowego pracownika: \n");
     scanf_s("%s", &nowy.Nazwisko, 31);
-    scanf_s("%d", &nowy.Pensja);
+    scanf_s("%lf", &nowy.Pensja);
     TabPrac[pozycja] = nowy;
+    pozycja++;
 }
 
-void opcja_W(Pracownik* TabPrac, int ilosc)
+void opcja_W(Pracownik* TabPrac, int pozycja)
 {
     printf("[Numer na liscie] Nazwisko - Pensja\n");
-    for (int i = 0; i < ilosc; i++)
+    for (int i = 0; i < pozycja; i++)
     {
-        printf("[%d] %s - %d\n", i + 1, TabPrac[i].Nazwisko, TabPrac[i].Pensja);
+        printf("[%d] %s - %lf\n", i + 1, TabPrac[i].Nazwisko, TabPrac[i].Pensja);
     }
+}
+
+void opcja_Z(Pracownik* TabPrac, int pozycja)
+{
+    char nazwa_pliku[32];
+    FILE* file_out = NULL;
+    Pracownik temp;
+
+    while (file_out == NULL)
+    {
+        printf("Podaj nazwe pliku: ");
+        scanf_s("%s", &nazwa_pliku, 31);
+        fopen_s(&file_out, nazwa_pliku, "w");
+    }
+
+    for (int i = 0; i < pozycja; i++)
+    {
+        fprintf(file_out, "%s ", TabPrac[i].Nazwisko, 31);
+        fprintf(file_out, "%lf\n", TabPrac[i].Pensja);
+    }
+
+    fclose(file_out);
 }
 
 int Z7_01()
@@ -37,7 +87,7 @@ int Z7_01()
 
     while (!end)
     {
-        printf("\nPodaj symbol opcji: \n");
+        printf("\nPodaj symbol opcji [R/N/W/Z/K]: ");
         opcja = vchar();
         opcja &= 0x5F;
 
@@ -45,12 +95,12 @@ int Z7_01()
         {
         case 'R':
         {
+            opcja_R(Pracownicy, ilosc);
             break;
         }
         case 'N':
         {
             opcja_N(Pracownicy, ilosc);
-            ilosc++;
             break;
         }
         case 'W':
@@ -60,6 +110,7 @@ int Z7_01()
         }
         case 'Z':
         {
+            opcja_Z(Pracownicy, ilosc);
             break;
         }
         case 'K':
