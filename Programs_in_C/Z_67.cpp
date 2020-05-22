06138ad642c967cc032485c83c0d8e2c15bc43b0
DarkHunter <konrad.romanski4@gmail.com>
2020-05-22 21:43:48 +0200

    bincode added

--- Programs_in_C/Z_67.cpp
+++ Programs_in_C/Z_67.cpp
@@ -1,10 +1,22 @@
 #include <stdio.h>
 #include <math.h>
 
+int bin2hex()
+{
+
+    return 1;
+}
+
+int hex2bin()
+{
+
+    return 1;
+}
 
 int Z_67()
 {
     char code[4];
+    int bincode[8] = {0};
     FILE* file_in;
     fopen_s(&file_in, "Z_67_in.txt", "r");
 
@@ -12,10 +24,43 @@ int Z_67()
 
     while (feof(file_in) == 0)
     {
+
+
+        for (int i = 0; i < 4; i++)
+        {
+            switch (code[i])
+            {
+            case 'A':
+            {
+                bincode[i * 2] = 0;
+                bincode[(i * 2) + 1] = 0;
+                break;
+            }
+            case 'C':
+            {
+                bincode[i * 2] = 0;
+                bincode[(i * 2) + 1] = 1;
+                break;
+            }
+            case 'G':
+            {
+                bincode[i * 2] = 1;
+                bincode[(i * 2) + 1] = 0;
+                break;
+            }
+            case 'T':
+            {
+                bincode[i * 2] = 1;
+                bincode[(i * 2) + 1] = 1;
+                break;
+            }
+            }
+        }
+
         for (int i = 0; i < 4; i++) printf("%c", code[i]);
         printf("\n");
-
-
+        for (int i = 0; i < 8; i++) printf("%d", bincode[i]);
+        printf("\n");
 
         fgetc(file_in);
         for (int i = 0; i < 4; i++) code[i] = fgetc(file_in);
