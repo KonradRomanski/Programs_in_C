6a44f5c874c1a52ed0df99a58a4715a6e289fe4d
DarkHunter <konrad.romanski4@gmail.com>
2020-05-24 16:08:15 +0200

    second option + custom file names added

--- Programs_in_C/Z_67.cpp
+++ Programs_in_C/Z_67.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 void bin2hex(int *bindata, char *hexval)
 {
@@ -47,38 +48,55 @@ int Z_67()
     char code[4];
     char hexcode[2] = {'0', '0'};
     int bincode[8] = {0};
-    FILE* file_in;
-    FILE* file_out;
+    FILE* file_in = NULL;
+    FILE* file_out = NULL;
     char data[32];
     char result[32];
     char opcja = ' ';
+    char letter[2];
+    bool ok = true;
 
     while (!(opcja == 'K' || opcja == 'D') && opcja != 'W')
     {
+        ok = true;
         printf("Wybierz opcje kodowanie/dekodowanie/wyjscie [k/d/w]: ");
         opcja = rchar();
         opcja &= 0x5F;
         if (opcja == 'K' || opcja == 'D')
         {
+            ok = false;
             printf("Wpisz nazwe pliku wejsciowego: ");
             scanf_s("%s", &data, 32);
-            printf("Wpisz nazwe pliku wyjsciowego: ");
-            scanf_s("%s", &result, 32);
+            fopen_s(&file_in, data, "r");
 
+            if (file_in == NULL)
+            {
+                printf("Plik o nazwie  %s  nie istnieje lub jest pusty, sprobuj jeszcze raz\n\n", data);
+                opcja = 'A';
+            }
+            else
+            {
+                printf("Wpisz nazwe pliku wyjsciowego: ");
+                scanf_s("%s", &result, 32);
+                if (strcmp(data, result) == 0)
+                {
+                    printf("Nazwy pliku wejsciowego i wyjsciowego musza byc rozne, sprobuj jeszcze raz\n\n");
+                    opcja = 'A';
+                }
+                else
+                {
+                    fopen_s(&file_out, result, "w");
+                    ok = true;
+                }
+
+            }
         }
 
-        fopen_s(&file_in, "Z_67_out.txt", "r");
-        fopen_s(&file_out, "Z_67_out2.txt", "w");
+        //fopen_s(&file_in, "w", "r");
+        //fopen_s(&file_out, "w", "w");
 
-        //fopen_s(&file_in, data, "r");
-        //fopen_s(&file_out, result, "w");
 
-        if (file_in == NULL && (opcja == 'K' || opcja == 'D'))
-        {
-            printf("Plik o nazwie  %s  nie istnieje, sprobuj jeszcze raz\n\n", data);
-            opcja = 'A';
-        }
-        else
+        if (ok)
         {
             switch (opcja)
             {
@@ -134,7 +152,8 @@ int Z_67()
                     else fprintf(file_out, "%c%c\n", hexcode[0], hexcode[1]);
 
                 }
-
+                fclose(file_in);
+                fclose(file_out);
                 break;
             }
             case 'D':
@@ -145,15 +164,66 @@ int Z_67()
                 {
                     hex2bin(hexcode, bincode);
 
+                    for (int i = 0; i < 4; i++)
+                    {
+                        letter[0] = bincode[i * 2];
+                        letter[1] = bincode[(i * 2) + 1];
+
+                        switch (letter[0])
+                        {
+                        case 0:
+                        {
+                            switch (letter[1])
+                            {
+                            case 0:
+                            {
+                                code[i] = 'A';
+                                break;
+                            }
+                            case 1:
+                            {
+                                code[i] = 'C';
+                                break;
+                            }
+                            }
+                            break;
+                        }
+                        case 1:
+                        {
+                            switch (letter[1])
+                            {
+                            case 0:
+                            {
+                                code[i] = 'G';
+                                break;
+                            }
+                            case 1:
+                            {
+                                code[i] = 'T';
+                                break;
+                            }
+                            }
+                            break;
+                        }
+                        }
+                    }
+
                     printf("Hexcode: ");
                     for (int i = 0; i < 2; i++) printf("%c", hexcode[i]); printf("\n");
                     printf("Bincode: ");
                     for (int i = 0; i < 8; i++) printf("%d", bincode[i]); printf("\n");
+                    for (int i = 0; i < 4; i++) printf("%c", code[i]); printf("\n");
                     printf("----\n");
+
+                    for (int i = 0; i < 4; i++) fprintf(file_out, "%c", code[i]);
+                    fprintf(file_out, "\n");
 
                     for (int i = 0; i < 2; i++) hexcode[i] = fgetc(file_in);
                     fgetc(file_in);
                 }
+
+                fclose(file_in);
+                fclose(file_out);
 
                 break;
             }
@@ -168,17 +238,8 @@ int Z_67()
                 break;
             }
             }
-
-            if (file_in != NULL)
-            {
-                fclose(file_in);
-                fclose(file_out);
-            }
         }
+
     }
-
-
-
-
     return 1;
 }
